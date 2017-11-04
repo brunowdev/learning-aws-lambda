141dbaad1be01a5d7cf63dddf21ba543a772e637
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-11-04 18:48:18 -0200

    refactoring

--- blog-app/articles/create.js
+++ blog-app/articles/create.js
@@ -2,44 +2,13 @@
 
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const uuid = require('uuid');
+const createArticle = require('./model.js').createArticle;
+const DynamoDAO = require('../utils/dynamo-dao.js');
+const ArticleController = require('./controller.js');
 
 module.exports.handler = (event, context, callback) => {
-	const data = JSON.parse(event.body);
-	if (!data.text || typeof data.text !== 'string') {
-		console.error('Validation failed');
-		callback(new Error('Body did not contain a text property.'));
-		return;
-	}
-	
-	let params = {
-		TableName: 'BlogTable',
-		Item: {
-			article_id: uuid.v1(),
-			text: data.text
-		}
-	};
-	
-	let putCbk = (error, result) => {
-
-       if (error) {
-	      console.log(error);
-		  callback(new Error('Could not save record.'));
-		  return;
-	   }	
-	   
-	   console.log(result);
-
-		const response = {
-			statusCode: 201,
-			body: JSON.stringify(result.Item)
-		};
-
-		callback(null, response);	   
-	
-	};
-	
-	dynamo.put(params, putCbk);
-	
- 
+    const article = createArticle(event, callback);
+    const dynamoDAO = new DynamoDAO(dynamo, 'BlogTable');
+    const controller = new ArticleController(dynamoDAO);
+    controller.createArticle(article, callback);
 };
