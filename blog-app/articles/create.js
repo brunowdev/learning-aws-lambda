7ed50b3a9bd9ed8f25e626bbd61f6c637f873171
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-11-04 17:55:35 -0200

    adicionado DynamoDB

--- blog-app/articles/create.js
+++ blog-app/articles/create.js
@@ -1,26 +1,45 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+
 module.exports.handler = (event, context, callback) => {
-	
-	const data = JSON.parse(event.body);
-	
-	if (data.text && typeof data.text !== 'string') {
+	const data = event;
+	if (!data.text || typeof data.text !== 'string') {
 		console.error('Validation failed');
 		callback(new Error('Body did not contain a text property.'));
 		return;
 	}
 	
-	// debug only
-	console.log(data.text);
-	
-	const response = {
-		statusCode: 201,
-		body: JSON.stringify({
-			  message: 'Created article.',
-			  input: event,
-			}),
+	let params = {
+		TableName: 'BlogTable',
+		Item: {
+			article_id: uuid.v1(),
+			text: data.text
+		}
 	};
+	
+	let putCbk = (error, result) => {
 
-	callback(null, response);
+       if (error) {
+	      console.log(error);
+		  callback(new Error('Could not save record.'));
+		  return;
+	   }	
+	   
+	   console.log(result);
+
+		const response = {
+			statusCode: 201,
+			body: JSON.stringify(result.Item)
+		};
+
+		callback(null, response);	   
+	
+	};
+	
+	dynamo.put(params, putCbk);
+	
  
 };
